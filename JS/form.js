b9e51229005e95b67d4d4d695d9610f908997108
ShanurFreelance <shanur.freelancer@gmail.com>
2024-11-29 22:29:28 +0530

    final in meetings

--- JS/form.js
+++ JS/form.js
@@ -2,7 +2,7 @@ const form = document.getElementById('contactForm');
         form.addEventListener('submit', async (e) => {
             e.preventDefault();
             const formData = new FormData(form);
-            const response = await fetch('https://script.google.com/macros/s/AKfycbwJv-PTojYvkhOMqUZccupCbncm_LZ08hkBNS4J4BioZp9a_fv3x03ctPc5KBQ5_wU/exec', {
+            const response = await fetch('https://script.google.com/macros/s/AKfycby07dwNX8o09BAQLcYDMFQ0-M-YsBc-lGORKJABfAN5xDYLyV8BiOeeLeFYgKUTypfgDw/exec', {
                 method: 'POST',
                 body: formData
             });

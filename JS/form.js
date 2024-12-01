00a284e1dcaa1211050d597e929db633a3f2c91e
ShanurFreelance <shanur.freelancer@gmail.com>
2024-12-01 22:43:15 +0530

    Update form.js
    url updated

--- JS/form.js
+++ JS/form.js
@@ -2,7 +2,7 @@ const form = document.getElementById('contactForm');
         form.addEventListener('submit', async (e) => {
             e.preventDefault();
             const formData = new FormData(form);
-            const response = await fetch('https://script.google.com/macros/s/AKfycby07dwNX8o09BAQLcYDMFQ0-M-YsBc-lGORKJABfAN5xDYLyV8BiOeeLeFYgKUTypfgDw/exec', {
+            const response = await fetch('https://script.google.com/macros/s/AKfycbxGnZjZ2K7xNQhEi_3C9zdUuRGtLfr4-OWlNRXeWzW69Q4ONUy15u6Bg-9g_0RZmcSSGw/exec', {
                 method: 'POST',
                 body: formData
             });
